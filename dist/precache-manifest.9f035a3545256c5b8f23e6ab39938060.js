15b5812b5531ee4321f3e9a18b9f06c09c25d6fe
Raza 9798 <m.raza9798@gmail.com>
2020-07-02 10:25:46 +0530

    updated the node script to run over heroku

---
+++ dist/precache-manifest.9f035a3545256c5b8f23e6ab39938060.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08e04b77f876ac468f6",
+    "url": "/css/app.c07a90d4.css"
+  },
+  {
+    "revision": "db9d5bb2708606890843",
+    "url": "/css/chunk-vendors.e99cff73.css"
+  },
+  {
+    "revision": "82a5603f132f6163b37d6741a17846f4",
+    "url": "/img/envirnment1.82a5603f.jpg"
+  },
+  {
+    "revision": "e9dabbf4bcd4f76d9ccf5779c152e864",
+    "url": "/img/services-banner.e9dabbf4.jpg"
+  },
+  {
+    "revision": "c4e8082672607f111ad1f724ce0de88f",
+    "url": "/img/sgs-22000.c4e80826.png"
+  },
+  {
+    "revision": "9f282419b275519f1ffe2d40b29cc5dc",
+    "url": "/img/slider-1_0.9f282419.jpg"
+  },
+  {
+    "revision": "2e095011d7ff80099ff16023abf2d6df",
+    "url": "/img/slider-2_1.2e095011.jpg"
+  },
+  {
+    "revision": "f31c8d5117012054555c4935a6dfb639",
+    "url": "/img/slider-3_1.f31c8d51.jpg"
+  },
+  {
+    "revision": "f9984248561287adce17ce9edd1e17de",
+    "url": "/img/slider-4.f9984248.jpg"
+  },
+  {
+    "revision": "041a274f35a65b6a10d282eb9e0d9013",
+    "url": "/img/slider-5.041a274f.jpg"
+  },
+  {
+    "revision": "ad52949f7188f5efa59b6885daa6a0dc",
+    "url": "/img/swiss.ad52949f.png"
+  },
+  {
+    "revision": "1252491d2c3a06b3da52a1a4fd2bd869",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08e04b77f876ac468f6",
+    "url": "/js/app.baee994c.js"
+  },
+  {
+    "revision": "db9d5bb2708606890843",
+    "url": "/js/chunk-vendors.15f079db.js"
+  },
+  {
+    "revision": "a2e49b3c010ba8c5cd2b2858ba468d85",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
